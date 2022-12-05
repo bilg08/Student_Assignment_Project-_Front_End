f0f2b56c0e04340ec4cfcb0938b17cf4cc232664
bilguunLeap2 <bilguunleap2@KEKW.local>
2022-12-05 13:23:18 +0800

    a

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ import { useLoaderContext, useUserContext } from "../context";
 import { Header, Footer, Modal } from "./index";
 import { Loader } from "./Loader";
 export const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://backend-leap2-production.up.railway.app/",
   headers: { Authorization: getCookie("token"),userId:getCookie('userId') },
 });
 export const LayOut = (props: { children: ReactNode }) => {
