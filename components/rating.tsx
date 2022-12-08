2080965077d46437793ecb4630fe347ca4f1d070
bilguunLeap2 <bilguunleap2@KEKW.local>
2022-12-08 13:01:33 +0800

    done

--- components/rating.tsx
+++ components/rating.tsx
@@ -20,7 +20,7 @@ export const Rate: React.FC<RateType> = ({ post, isRating }:any) => {
     <motion.div
      initial={{ x: 100, opacity: 0 }}
      animate={isRating?{ x: 0, opacity: 1 }:{ x: 100, opacity: 0 }}
-     className="items-center justify-center w-full flex bg-white flex-col justify-between font-medium text-center    h-[auto]    ">
+     className="items-center justify-center w-full flex flex-col justify-between font-medium text-center bg-white   h-[auto]    ">
       <Slider defaultValue={state} sx={{width:'60%'}} onChange={(e:any) => setState(e.target.value)} aria-label="Default" valueLabelDisplay="auto" />
       <button onClick={rateWorkerPerformance}>Батлах</button>
     </motion.div>
